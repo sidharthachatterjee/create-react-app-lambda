c93a1b5ed8fab61a6cd4a7186f8eab345dd34d6c
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2019-01-11 18:36:20 +0530

    Add Gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+var proxy = require('http-proxy-middleware')
+
+module.exports = {
+  developMiddleware: app => {
+    app.use(
+      '/.netlify/functions/',
+      proxy({
+        target: 'http://localhost:9000',
+        pathRewrite: {
+          '/.netlify/functions/': ''
+        }
+      })
+    )
+  }
+}
